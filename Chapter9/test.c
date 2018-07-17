c2da28eb517d9db4f89c4f5758fc69ff5d7effb8
basstal <330475004@qq.com>
2018-07-18 07:56:00 +0800

    Chapter9 update

--- Chapter9/test.c
+++ Chapter9/test.c
@@ -2,8 +2,9 @@
 
 int main(int argc, char ** argv)
 {
-    FILE * fd = fopen(argv[1], "a+");
-    mmap(stdout, fd->_bf._size, PROT_WRITE, MAP_PRIVATE, fd->_file, 0);
-    fflush(stdout);
+    int fileno = open(argv[1], O_RDWR);
+    // printf(fd)
+    void * bufp = mmap(NULL, 1, PROT_READ, MAP_PRIVATE, fileno, 0);
+    printf("%s", bufp);
     exit(0);
 }
